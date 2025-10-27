f87d731240480bf914ce7f6fe2a58d59131f28cb
natanferreirah <natanferreiradon31@gmail.com>
2025-10-27 10:15:31 -0300

    :sparkles: feat: Resource de CRUD Turmas

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SchoolClassController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('students', StudentController::class);
+Route::resource('school-classes', SchoolClassController::class);
