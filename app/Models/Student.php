264cf48318645d8c353f34b337fc803d6601c0df
natanferreirah <natanferreiradon31@gmail.com>
2025-10-27 10:08:22 -0300

    :sparkles: feat: Relação com a model SchoolClass

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,13 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\SchoolClass;
 
 class Student extends Model
 {
-    protected $fillable = [ 
+    use HasFactory;
+
+    protected $fillable = [
         'name',
         'cpf',
-        'birth_date'
+        'birth_date',
+        'school_class_id',
     ];
+
+    public function schoolClass()
+    {
+        return $this->belongsTo(SchoolClass::class);
+    }
 }
