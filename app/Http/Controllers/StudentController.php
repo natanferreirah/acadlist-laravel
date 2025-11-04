969a7a79f779abd4932fe2f66570354cd073e4a1
natanferreirah <natanferreiradon31@gmail.com>
2025-11-03 23:41:36 -0300

    :bug: fix: Correção de erros

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -11,13 +11,13 @@ class StudentController extends Controller
     public function index()
     {
         $students = Student::with('schoolClass')->get();
-        return view('student.index', compact('students'));
+        return view('students.index', compact('students'));
     }
 
     function create()
     {
-        $schoolClasses = SchoolClass::all();
-        return view('student.create', compact('schoolClasses'));
+        $schoolclasses = SchoolClass::all();
+        return view('students.create', compact('schoolclasses'));
     }
 
     public function store(Request $request)
@@ -45,10 +45,10 @@ class StudentController extends Controller
 
     public function edit(Student $student)
     {
-        $schoolClasses = SchoolClass::all();
-        $edited = Student::findorfail($student->id);
+        $schoolclasses = SchoolClass::all();
+        $student->id;
 
-        return view('student.edit',  compact('student', 'schoolClasses'));
+        return view('students.edit',  compact('student', 'schoolclasses'));
     }
 
     public function update(Request $request, Student $student)
@@ -60,9 +60,8 @@ class StudentController extends Controller
             'school_class_id' => 'required|exists:school_classes,id',
         ]);
 
-        $students = Student::findorfail($student->id);
 
-        $updated = $students->update($request->all());
+        $updated = $student->update($request->all());
 
         if ($updated) {
             return redirect()->route('students.index')->with('sucess', 'Aluno atualizado com sucesso');
@@ -73,9 +72,8 @@ class StudentController extends Controller
 
     public function destroy(Student $student)
     {
-        $students = Student::findorfail($student->id);
 
-        $deleted = $students->delete();
+        $deleted = $student->delete();
 
         if ($deleted) {
             return redirect()->route('students.index')->with('sucess', 'Aluno excluído com sucesso');
