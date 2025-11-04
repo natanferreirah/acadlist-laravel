cd7f8b929c45c3c24aaab5d3c801772c22f16b50
natanferreirah <natanferreiradon31@gmail.com>
2025-11-03 23:39:53 -0300

    :bug: fix: Correção de erros

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acadlist - Alunos</title>
+</head>
+
+<body>
+    <h1>Alunos</h1>
+    <a href="{{route('students.create')}}">Cadastrar</a>
+    @foreach($students as $student)
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>CPF</th>
+            <th>Data de Nascimento</th>
+            <th>Turma</th>
+            <th>Série</th>
+            <th>Turno</th>
+        </tr>
+        <tr>
+            <td>{{ $student->id }}</td>
+            <td>{{ $student->name }}</td>
+            <td>{{ $student->cpf_formatado }}</td>
+            <td>{{ $student->birth_date }}</td>
+            <td>{{ $student->schoolClass->name ?? 'Sem turma'}}</td>
+            <td>{{ $student->schoolClass->grade ?? '-'}}</td>
+            <td>{{ $student->schoolClass->shift_label ?? '-'}}</td>
+            <td><a href="{{route('students.edit', $student->id)}}">Editar</a></td>
+            <td>
+                <form action="{{ route('students.destroy', $student->id) }}" method="post">
+                    @method('delete')
+                    @csrf
+                    <input type="submit" value="Excluír" onclick="return confirm('Tem certeza que deseja excluír o aluno {{ $student->name }}?')">
+                </form>
+            </td>
+        </tr>
+    </table>
+    @endforeach
+</body>
+
+</html>
