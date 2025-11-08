5f81da60c47a4b5789dca53fa99fd893364af2ce
natanferreirah <natanferreiradon31@gmail.com>
2025-11-08 00:42:38 -0300

    Rota de Notas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GradeController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\TeacherController;
@@ -14,4 +15,5 @@ Route::resource('students', StudentController::class);
 Route::resource('school-classes', SchoolClassController::class);
 Route::resource('teachers', TeacherController::class);
 Route::resource('subjects', SubjectController::class);
+Route::resource('grades', GradeController::class);
 
