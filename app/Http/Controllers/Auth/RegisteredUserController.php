3861a786e74f3845933d821859a11e756d783638
natanferreirah <natanferreiradon31@gmail.com>
2025-11-14 23:43:04 -0300

    :sparkles: feat: Adiciona Laravel breeze

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisteredUserController extends Controller
+{
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|max:255',
+            'name_school' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'name_school' => $request->name_school,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'school', // fixo
+        ]);
+
+        event(new Registered($user));
+        Auth::login($user);
+
+        return redirect()->route('dashboard')->with('success', 'Escola registrada com sucesso!');
+    }
+}
