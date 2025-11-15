794b3b05464d4a7e72a2f65739a659e8e41e65a2
natanferreirah <natanferreiradon31@gmail.com>
2025-11-15 00:01:53 -0300

    fix: Corrigir erro do  nível de conta

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GradeController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\TeacherController;
@@ -11,9 +12,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('students', StudentController::class);
-Route::resource('school-classes', SchoolClassController::class);
-Route::resource('teachers', TeacherController::class);
-Route::resource('subjects', SubjectController::class);
-Route::resource('grades', GradeController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'role:school'])->group(function () {
+    Route::resource('students', StudentController::class);
+    Route::resource('school-classes', SchoolClassController::class);
+    Route::resource('teachers', TeacherController::class);
+    Route::resource('subjects', SubjectController::class);
+    Route::resource('grades', GradeController::class);
+});
+
+Route::middleware(['auth', 'role:teacher,school'])->group(function () {
+    Route::resource('grades', GradeController::class);
+});
+
+
+
+require __DIR__ . '/auth.php';
