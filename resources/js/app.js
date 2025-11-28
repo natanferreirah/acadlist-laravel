b3caa2a8a52d692b719e6a91737380b3409637c5
natanferreirah <natanferreiradon31@gmail.com>
2025-11-28 12:06:00 -0300

    feat: Adicionar Alpine.JS para utilização de máscara

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,20 @@
-import './bootstrap';
+import Alpine from 'alpinejs'
+import IMask from 'imask'
 
-import Alpine from 'alpinejs';
+// Máscaras
+document.addEventListener('alpine:init', () => {
+    Alpine.directive('mask-cpf', (el) => {
+        IMask(el, { mask: '000.000.000-00' });
+    });
+    
+    Alpine.directive('mask-phone', (el) => {
+        IMask(el, {
+            mask: [
+                { mask: '(00) 00000-0000' }
+            ]
+        });
+    });
+});
 
-window.Alpine = Alpine;
-
-Alpine.start();
+window.Alpine = Alpine
+Alpine.start()
