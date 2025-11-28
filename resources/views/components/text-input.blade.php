689302165e629e2188314e787c9de38873270b7e
natanferreirah <natanferreiradon31@gmail.com>
2025-11-28 12:01:19 -0300

    fix: Corrigir desing

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 border-gray-200 bg-gray-50 text-gray-800 focus:border-blue-500 dark:focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 border-gray-200 bg-gray-50 text-gray-900 focus:border-blue-700 focus:ring-blue-500  rounded-md shadow-sm']) }}>
